4e198e56dd4a25af5b5cc48f5883a85f320407a2
xingjiudong <25635680@qq.com>
2017-09-07 11:56:40 +0800

    add  to etcd

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 : ${ETCD_CLIENT_IP:=127.0.0.1}
+: ${{PROJECT_NAME:=}
 
 # Make sure service is running
 service rsyslog start
@@ -13,8 +14,11 @@ touch /var/log/haproxy.log
 tail -f /var/log/haproxy.log &
 
 # Create haproxy.cfg
-mkdir -p /usr/local/etc/haproxy
-confd -onetime -backend etcd -node http://${ETCD_CLIENT_IP}:2379
+sed -r "s/@PROJECT_NAME@/${PROJECT_NAME}/g" /etc/confd/templates/haproxy.cfg.tmpl.in > /etc/confd/templates/haproxy.cfg.tmpl
+
+sed -r "s/@PROJECT_NAME@/${PROJECT_NAME}/g" /etc/confd/conf.d/haproxy.cfg.toml.in > /etc/confd/conf.d/haproxy.cfg.toml
+
+confd -onetime -backend etcd -node http://${ETCD_CLIENT_IP}:2379 --prefix="/haproxy-config"
 
 # Start haproxy
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg
